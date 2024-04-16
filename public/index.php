0ef9cf1dfbfb813c356c1c9d73c60406e0a9f125
affonja <56819481+affonja@users.noreply.github.com>
2024-04-17 00:58:15 +0400

    step3_04

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ use DI\Container;
 use App\Database;
 
 require __DIR__ . '/../vendor/autoload.php';
-
+session_start();
 
 $pdo = new Database();
 $urls = $pdo->createTable();
@@ -18,9 +18,15 @@ $container = new Container();
 $container->set('renderer', function () {
     return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
 });
+$container->set('flash', function () {
+    return new \Slim\Flash\Messages();
+});
+AppFactory::setContainer($container);
+
 $app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 $router = $app->getRouteCollector()->getRouteParser();
+
 
 $app->get('/', function ($request, $response) {
     return $this->get('renderer')->render($response, 'main.phtml');
@@ -39,17 +45,26 @@ $app->post('/urls', function ($request, $response) use ($pdo) {
     $isExist = !(($pdo->query($sql) === null));
     if ($isExist) {
         $id = $pdo->query($sql)[0]['id'];
+        $this->get('flash')->addMessage('success', 'Страница уже существует');
         return $response->withStatus(302)->withHeader('Location', "/urls/$id");
     }
     $idNew = $pdo->insert($siteUrl);
+    $this->get('flash')->addMessage('success', 'Страница успешно добавлена');
     return $response->withStatus(302)->withHeader('Location', "/urls/$idNew");
-})->setName('url');
+})->setName('urlpost');
 
 $app->get('/urls/{id}', function ($request, $response, $args) use ($pdo) {
+    $messages = $this->get('flash')->getMessages();
+
     $sql = "select * from urls where id={$args['id']}";
     $query = $pdo->query($sql)[0];
-    $params = ['id' => $query['id'], 'siteUrl' => $query['name'], 'created' => $query['created_at']];
+    $params = [
+        'id' => $query['id'],
+        'siteUrl' => $query['name'],
+        'created' => $query['created_at'],
+        'flash' => $messages
+    ];
     return $this->get('renderer')->render($response, 'url.phtml', $params);
-})->setName('urls2');
+})->setName('url');
 
 $app->run();
